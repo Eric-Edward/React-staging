c7ff729c6a9ccbf315347d44d8b985b002ef8e49
Eric-Edward <EricEdward.Pro@gmail.com>
2021-09-10 18:25:21 +0800

    master

--- src/pages/Home/Message/Detail/index.jsx
+++ src/pages/Home/Message/Detail/index.jsx
@@ -1,42 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-// import qs from 'querystring'
+import qs from "querystring";
 
 class Detail extends Component {
+  state = {
+    messages: [
+      { id: "001", content: "你好，世界！" },
+      { id: "002", content: "哈哈哈哈！" },
+      { id: "003", content: "我爱学习！" },
+    ],
+  };
 
-    state = {
-        messages: [
-            {id: '001', content: '你好，世界！'},
-            {id: '002', content: '哈哈哈哈！'},
-            {id: '003', content: '我爱学习！'}
-        ]
-    }
+  render() {
+    console.log(this.props);
+    //通过params进行传参
+    // const {id, title} = this.props.match.params;
 
-
-    render() {
-        console.log(this.props);
-        // const {id, title} = this.props.match.params;
-
-
-        /*const {search} = this.props.location;
+    //通过search进行传参,这里silce是因为这里前面有一个？
+    const { search } = this.props.location;
+    const { id, title } = qs.parse(search.slice(1));
+    /*const {search} = this.props.location;
         const {id, title} = qs.parse(search.slice(1));*/
 
-        const {id, title} = this.props.location.state || {};
+    // const {id, title} = this.props.location.state || {};
 
-        const findResult = this.state.messages.find((message) => {
-            return message.id === id;
-        }) || {};
+    const findResult =
+      this.state.messages.find((message) => {
+        return message.id === id;
+      }) || {};
 
-        return (
-            <div>
-                <ul>
-                    <li>ID:{id}</li>
-                    <li>TITLE:{title}</li>
-                    <li>CONTENT:{findResult.content}</li>
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <ul>
+          <li>ID:{id}</li>
+          <li>TITLE:{title}</li>
+          <li>CONTENT:{findResult.content}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Detail;
